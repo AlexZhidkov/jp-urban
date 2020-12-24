e7325d2e292c572a3594243e39fcf0bba98b89a2
Alexander Zhidkov <azhidkov@gmail.com>
2020-12-24 15:34:24 +0800

    show list of events from calendar

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -9,13 +9,25 @@ import { Observable } from 'rxjs';
 })
 export class BookingComponent implements OnInit {
   data: Observable<any>;
+  calendarEvents: any[];
+  isLoading: boolean;
 
   constructor(private fns: AngularFireFunctions) { }
 
   ngOnInit(): void {
-    const callable = this.fns.httpsCallable('getListOfEvents');
-    this.data = callable({});
-    this.data.subscribe(d => { debugger; });
+    this.isLoading = true;
+    const getListOfEvents = this.fns.httpsCallable('getListOfEvents');
+    getListOfEvents({}).subscribe(d => {
+      this.calendarEvents = d.items;
+      this.calendarEvents.forEach(i => {
+        i.startDate = new Date(i.start.dateTime).toDateString();
+        i.startTime = `${new Date(i.start.dateTime).getHours()}:${(new Date(i.start.dateTime).getMinutes() + '00').substr(0, 2)}`;
+        i.endDate = new Date(i.end.dateTime).toDateString();
+        if (i.startDate === i.endDate) { i.endDate = ''; }
+        i.endTime = `${new Date(i.end.dateTime).getHours()}:${(new Date(i.end.dateTime).getMinutes() + '00').substr(0, 2)}`;
+      });
+      this.isLoading = false;
+    });
   }
 
 }
